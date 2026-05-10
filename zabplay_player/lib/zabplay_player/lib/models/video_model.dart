8a0a5f66f7f87cdad0d34a148c637ba4c0e21318
israja853-ops <israja853@gmail.com>
2026-05-10 14:28:01 +0530

    Create video_model.dart

---
+++ zabplay_player/lib/zabplay_player/lib/models/video_model.dart
@@ -0,0 +1,11 @@
+class VideoModel {
+  final String title;
+  final String path;
+  final String thumbnail;
+
+  VideoModel({
+    required this.title,
+    required this.path,
+    required this.thumbnail,
+  });
+}

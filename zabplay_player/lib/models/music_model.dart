95e4ecfd2359631c5e79f2e882e232575e5c6004
israja853-ops <israja853@gmail.com>
2026-05-10 14:29:19 +0530

    Create music_model.dart

---
+++ zabplay_player/lib/models/music_model.dart
@@ -0,0 +1,13 @@
+class MusicModel {
+  final String title;
+  final String artist;
+  final String path;
+  final String image;
+
+  MusicModel({
+    required this.title,
+    required this.artist,
+    required this.path,
+    required this.image,
+  });
+}

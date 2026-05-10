69b939475f540509197b8b1e400c312ca4d07cde
israja853-ops <israja853@gmail.com>
2026-05-10 13:52:55 +0530

    Create video_model.dart

---
+++ lib/models/video_model.dart
@@ -0,0 +1,13 @@
+class VideoModel {
+  final String title;
+  final String path;
+  final String thumbnail;
+  final Duration duration;
+
+  VideoModel({
+    required this.title,
+    required this.path,
+    required this.thumbnail,
+    required this.duration,
+  });
+}
